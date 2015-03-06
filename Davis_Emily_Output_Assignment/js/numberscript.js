2d90b2cdf39c006fdff2556bb87874a8014985b9
AuntieEm <Emily.davis.5828@gmail.com>
2015-03-06 02:43:43 -0600

    Attempting Successful Number Variable
    99 little bugs in my code….
    99 little bugs… Take one out, move it around, 127 little bugs in my
    code….

---
+++ Davis_Emily_Output_Assignment/js/numberscript.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Alys1 on 3/5/15.
+ */
+//Emily Davis - Output Assignment - Scalable Data Infrastructures - MDV2330-0 - March 5, 2015
+//Number Variable
+
+
+var yrsHooman = prompt("Enter the degrees in Fahrenheit", 50);
+var yrsDog;
+
+yrsDog = 5/9 * (degFahren -32);
+
+alert(yrsHooman + "\xB0 Fahrenheit is " + yrsDog + "\xB0 centigrade")
