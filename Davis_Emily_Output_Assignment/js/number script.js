daf124ec1c95f84514e94e8894ef9e4b41767aae
AuntieEm <Emily.davis.5828@gmail.com>
2015-03-06 01:33:10 -0600

    Organization update
    embedded a script source in the index html. Starting the number script.

--- Davis_Emily_Output_Assignment/js/number script.js
+++ Davis_Emily_Output_Assignment/js/number script.js
@@ -3,3 +3,10 @@
  */
 //Emily Davis - Output Assignment - Scalable Data Infrastructures - MDV2330-0 - March 5, 2015
 //Number Variable
+    
+var degFahren = prompt("Enter the degrees in Fahrenheit", 50);
+var degCent;
+
+degCent = 5/9 * (degFahren -32);
+
+alert(degFahren + "\xB0 Fahrenheit is " + degCent + "\xB0 centigr
