ce8c730a6cbc6eb8f85f7a4fc6c5ae6bb9558cbd
AuntieEm <Emily.davis.5828@gmail.com>
2015-03-05 23:42:39 -0600

    Completed String Script With Notes
    Used 4 strings as alerts to form a sentence as they pop up.

--- Davis_Emily_Output_Assignment/js/string script.js
+++ Davis_Emily_Output_Assignment/js/string script.js
@@ -3,3 +3,19 @@
  */
 //Emily Davis - Output Assignment - Scalable Data Infrastructures - MDV2330-0 - March 5, 2015
 //String Variable
+
+var popup1 = "Did you know...";
+//declaring the existence of the first variable and what it is called. Assigning a value to the variable.
+alert(popup1);
+
+var popup2 = "...that I love to design...";
+//declaring the existence of the second variable and what it is called. Assigning a value to the variable.
+alert(popup2);
+
+var popup3 = "...AND develop?";
+//declaring the existence of the third variable and what it is called. Assigning a value to the variable.
+alert(popup3);
+
+var popup4 = "Weird. Huh?";
+//declaring the existence of the fourth variable and what it is called. Assigning a value to the variable.
+alert(popup4);
