03aa4fa3a658274f1c8dfe08ca7532e16eca07dd
AuntieEm <Emily.davis.5828@gmail.com>
2015-03-11 00:08:27 -0500

    Dog Years
    Calculate how old Sparky is in dog years.

---
+++ Davis_Emily_Expressions_worksheet/js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Alys1 on 3/10/15.
+ */
+//Dog Years
+
+var dogYears = 7; //7 dog years to 1 human year
+var sparkyAge = prompt("How old is Sparky in human years?"); //prompts user to provide a number.
+var result = sparkyAge * dogYears; //calculates how old sparky is.
+
+console.log("Sparky is " + result + " " + "years old in dog years.");
+
