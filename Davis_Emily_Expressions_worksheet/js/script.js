a6a67be782f7fd2621d41a77ea9fefb346994219
AuntieEm <Emily.davis.5828@gmail.com>
2015-03-11 01:02:18 -0500

    Average Shopping Bill
    Calculate how much is spent in five weeks and what the average is.

--- Davis_Emily_Expressions_worksheet/js/script.js
+++ Davis_Emily_Expressions_worksheet/js/script.js
@@ -14,7 +14,7 @@ console.log("Sparky is " + sparkyAge + " " + "in human years. Which is " + resul
 
 //Slice of Pie part 1
 
-
+/*
 var peopleAtParty = prompt ("How many people are attending the party?");
 var pizzaOrdered = prompt ("How many pizzas did you order?");
 var slicePerPizza = prompt ("How many slices is each pizza divided up into?");
@@ -30,4 +30,23 @@ console.log("Each person ate" + " " + result + " " + "slices of pizza at the par
 var sparkyNom = (slicePerPizza * pizzaOrdered) % peopleAtParty;
 
 console.log("Sparky got " + sparkyNom + " " + "slices of pizza.");
+*/
+
+//Average shopping bill
+/*
+var weak1 = prompt ("How much did you spend on groceries 5 weeks ago?")
+var weak2 = prompt ("4 weeks ago?");
+var weak3 = prompt ("3 weeks ago?");
+var weak4 = prompt ("2 weeks ago?");
+var weak5 = prompt ("1 week ago?");
+
+
+var groceryTotals = [Number(weak1), Number(weak2), Number(weak3), Number(weak4), Number(weak5)];
+var total = Number(groceryTotals[0]) + Number(groceryTotals[1]) + Number(groceryTotals[2]) + Number(groceryTotals[3]) + Number(groceryTotals[4])
+var average = total / 5;
+
+console.log("You have spent a total of $" + total + " " + "on groceries over 5 weeks.")
+console.log("That is an average of $" + average + " " + "per week.");
+*/
+
 
