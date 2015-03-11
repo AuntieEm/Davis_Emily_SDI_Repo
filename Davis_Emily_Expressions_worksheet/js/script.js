14241a0a6302fd27d9694785b21f9a808b8506ff
AuntieEm <Emily.davis.5828@gmail.com>
2015-03-11 00:35:20 -0500

    Slice of Pie Part 2
    calculate how many slices of pizza sparky got after everyone else got
    their pizza.
    Update part 1.

--- Davis_Emily_Expressions_worksheet/js/script.js
+++ Davis_Emily_Expressions_worksheet/js/script.js
@@ -8,22 +8,26 @@ var dogYears = 7; //7 dog years to 1 human year
 var sparkyAge = prompt("How old is Sparky in human years?"); //prompts user to provide a number.
 var result = sparkyAge * dogYears; //calculates how old sparky is.
 
-console.log("Sparky is " + sparkyAge + " " + "in human years. Whis is " + result + " " + "in dog years.");
+console.log("Sparky is " + sparkyAge + " " + "in human years. Which is " + result + " " + "in dog years.");
 */
 
 
 //Slice of Pie part 1
 
-/*
-A bunch of students are having a party and somebody ordered a pizza. Create an expression that calculates
-how much pizza each partygoer will get at the party. (Assume all pizzas have the same number of slices
-and that the person dividing the pizza is really precise, so this can be a decimal, like 3.52 slices, etc.)
- */
 
 var peopleAtParty = prompt ("How many people are attending the party?");
 var pizzaOrdered = prompt ("How many pizzas did you order?");
-var slicePerPizza = prompt ("How many slices are each pizza divided up into?");
+var slicePerPizza = prompt ("How many slices is each pizza divided up into?");
 
-var result = (peopleAtParty * 3) / (slicePerPizza) / (pizzaOrdered);
+var result = (pizzaOrdered * slicePerPizza) / peopleAtParty;
 
 console.log("Each person ate" + " " + result + " " + "slices of pizza at the party.");
+
+
+
+//Slice of Pie Part 2
+
+var sparkyNom = (slicePerPizza * pizzaOrdered) % peopleAtParty;
+
+console.log("Sparky got " + sparkyNom + " " + "slices of pizza.");
+
