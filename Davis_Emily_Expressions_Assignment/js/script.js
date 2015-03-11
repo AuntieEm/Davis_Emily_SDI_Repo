856b64de7aae4d3cc08bae9f62d8bd243a2da728
AuntieEm <Emily.davis.5828@gmail.com>
2015-03-11 05:13:27 -0500

    Assignment Operators
    included 2 assignment operators examples.

--- Davis_Emily_Expressions_Assignment/js/script.js
+++ Davis_Emily_Expressions_Assignment/js/script.js
@@ -10,6 +10,7 @@ var zoneUp = 10 / 100;  //10% up charge
 var dogCost = 15;   //per dog per day
 var catCost = 10;   //per cat per day
 
+
 //prompted variables
 
 var zoneNum = prompt ("According to the Zones page on my website \nwhich zone do you live in?", 3); //user input for thier zone each zone has a different upcharge.
@@ -19,6 +20,7 @@ var nightsNum = prompt ("How many nights do you need me to stay?", 13); //user i
 
 //calculated prompted variables
 
+
 var subTotal = ((Number(dogNum) * Number(dogCost) * Number(nightsNum)) + (Number(catNum) * Number(catCost) * Number(nightsNum)));
 //casts variables and calculates a subtotal for base cost
 var perDay = Number(subTotal) / Number(nightsNum);
@@ -27,13 +29,24 @@ var discount = discountUnits * perDay; //calculates discount amount
 var zoneCost = subTotal * zoneUp; //calculates zone cost up charge amount
 var total = (subTotal + zoneCost) - (discount); //calculates the total cost
 
+var d = Number(dogCost); //d is for dogs. the cost is 15 per dog.
+d *= Number(dogNum); // calculates for $15 per night for 3 dogs.
+
+var c = Number(catCost); //c is for cats. the cost is 10 per cat.
+c *= Number(catNum); // calculates for $10 per night for 1 cat.
+
 
 //Outputs
+
 
 console.log("My client lives in Zone " + zoneNum + ".");
 //states which zone my client lives in.
 console.log("She has " + dogNum + " " + "dogs and " + catNum + " " + "cats.");
 //states how many cats and dogs my client has.
+console.log("It will cost $" + d + " " + "per day for the dogs.");
+//states how much it will be for the dogs per day.
+console.log("It will cost $" + c + " " + "per day for the cats");
+//calculates how much it will be for the cats per day.
 console.log("My prices are: $" + dogCost + " " + "per dog per night and $" + catCost + " " + "per cat per night.");
 //states my prices for cats and dogs per night.
 console.log ("She needs my services for " + nightsNum + " " + "nights.");
@@ -46,3 +59,8 @@ console.log("She also qualfies for the 'Book 7 nights and get 1 free' discount. 
 //calculates the discount amount.
 console.log("Her total cost now is $ " + total + ".");
 //calculates final cost for my services.
+
+
+
+
+
