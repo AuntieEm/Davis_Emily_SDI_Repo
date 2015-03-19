df7cabb4a655076750935b9bbc2053c96118db41
AuntieEm <Emily.davis.5828@gmail.com>
2015-03-19 01:26:52 -0500

    update1
    4 variables
    2 outputs
    1 validation

--- Davis_Emily_Conditionals_Assignment/js/script.js
+++ Davis_Emily_Conditionals_Assignment/js/script.js
@@ -4,3 +4,25 @@ March 19, 2015
 Conditionals Assignment
  */
 
+//variables
+
+
+
+var city        = prompt("What city do you live in?","Grapevine");
+var milesToAirport = prompt("How many miles is it to the nearest airport?",10);
+var days        = prompt("How many days will you be needing my services?");
+var petType     = prompt("What kind of pet(s) do you have?","Dogs or cats?");
+
+
+//outputs
+
+console.log(city + " is " + milesToAirport + " miles away from the nearest airport.");
+
+
+
+
+//validation
+if(days === "")  {
+    days = prompt("You didn't specify how long you need my services for.")
+    console.log("You'll be needing my services for " + days + " days.");
+}
