d93b3065f0ee4ea98c8545077b10904256ff6f04
AuntieEm <Emily.davis.5828@gmail.com>
2015-03-19 09:51:58 -0500

    3rd prompt
    added 3rd calculating prompt

--- Davis_Emily_Conditionals_Assignment/js/script.js
+++ Davis_Emily_Conditionals_Assignment/js/script.js
@@ -8,6 +8,7 @@ Conditionals Assignment
 var dogPerson     = true;
 var catPerson   = true;
 var dogPrice = 30;
+var catPrice = 15;
 var days        = prompt("How many days will you be needing my services?");
 if(days === "")  {
     days = prompt("You didn't specify how long you need my services for.",3);
@@ -21,7 +22,11 @@ days = Number(days);
 catPerson = confirm("Are you a cat person? Click Okay for true and cancel for false.");
 
 if (catPerson === true) {
-       console.log("I'm sorry. I don't pet sit for cats.");
+       var cats = prompt("How many cats do you have?");
+    if(cats === "") {
+        cats = prompt("You didn't tell me how many cats you have.")
+        cats = Number(cats);
+    }
 }else{
     console.log("Well, okay then...")}
 
@@ -31,7 +36,7 @@ dogPerson = confirm("Are you a dog person? Click Okay for true and cancel for fa
 if (dogPerson === true) {
     var dogs = prompt("How many dogs do you have?");
     if (dogs === "") {
-        dogs = prompt("You did not tell me how many dogs you have.", 3);
+        dogs = prompt("You did not tell me how many dogs you have.");
         dogs = Number(dogs);
     }
 }else{
@@ -39,7 +44,10 @@ if (dogPerson === true) {
 }
 
 //more variables
-var total = dogs * dogPrice * days;
+var dogTotal = dogs * dogPrice * days;
+var catTotal = cats * catPrice * days;
+var total = dogTotal + catTotal;
+total = Number(total);
 var weekDiscount = ((days / 7) * (total / days));
 weekDiscount = Number(weekDiscount);
 var newTotal = (total - weekDiscount);
@@ -49,14 +57,14 @@ newTotal = Number(newTotal);
 console.log("Your total cost is $" + total + ".");
 
 //discounts
-(days > 7) ? console.log("Congratz! You qualify for the 'Book 7 get 1' discount!") : console.log("You did not book enough days to earn the 'Book 7 get 1' discount.");
+(days >= 7) ? console.log("Congratz! You qualify for the 'Book 7 get 1' discount!") : console.log("You did not book enough days to earn the 'Book 7 get 1' discount.");
 
 if (days >= 7){
     console.log("You booked us for " + days + " days, you get a discount of $" + weekDiscount + ".");
     console.log("You're new total is $" + newTotal + ".");
 
-}else if (dogs >= 4 && days <7){
-    console.log("You booked us to take care of " + dogs + " dogs, so you get 10% off your total.");
+}else if ((dogs + cats) >= 4 && days <7){
+    console.log("You booked us to take care of a total of " + (Number(dogs) + Number(cats)) + " animals, so you get 10% off your total.");
     console.log("Which is $" + tenPercent + ".");
     console.log("You're new total is $" + (total - tenPercent) + ".");
 } else{
