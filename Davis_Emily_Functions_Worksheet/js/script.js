76579832b79e249156c253bb885ab4dd0cedfb22
AuntieEm <Emily.davis.5828@gmail.com>
2015-03-24 23:13:07 -0500

    Circumference
    calculated the circumference of a circle

--- Davis_Emily_Functions_Worksheet/js/script.js
+++ Davis_Emily_Functions_Worksheet/js/script.js
@@ -7,6 +7,7 @@ Functions Worksheet
 
 //Example
 //caculate the area of a rectangle given the width and height of the rectangle.
+console.log("Example:  Calculate the area of a rectangle given the width and height of the rectangle.");
 
 var width = 5;
 var height = 6;
@@ -15,4 +16,19 @@ console.log("The Area of the rectangle is " + area + ".");
 
 function calculateArea(w, h) {
     return w * h;
+}
+
+console.log(" ");
+
+//Circumference
+console.log("Calculate the circumference of a circle.");
+
+var pi = 3.14;
+var radius = prompt("Please enter the radius of the circle.");
+radius = Number(radius);
+var circumference = calculateCirc(pi, radius)
+console.log("The circumference of the circle is " + circumference + ".");
+
+function calculateCirc(p, r) {
+    return 2 * p * r;
 }
