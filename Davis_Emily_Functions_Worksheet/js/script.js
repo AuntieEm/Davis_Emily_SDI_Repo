a82ebf90fad01748be9dfb6901d048855a4c99c9
AuntieEm <Emily.davis.5828@gmail.com>
2015-03-24 23:18:26 -0500

    stung start
    determined variables

--- Davis_Emily_Functions_Worksheet/js/script.js
+++ Davis_Emily_Functions_Worksheet/js/script.js
@@ -26,9 +26,21 @@ console.log("Calculate the circumference of a circle.");
 var pi = 3.14;
 var radius = prompt("Please enter the radius of the circle.");
 radius = Number(radius);
-var circumference = calculateCirc(pi, radius)
+var circumference = calculateCirc(pi, radius);
 console.log("The circumference of the circle is " + circumference + ".");
 
 function calculateCirc(p, r) {
     return 2 * p * r;
 }
+
+console.log(" ");
+
+//Stung
+console.log("It takes 8.6666666667 bee stings per pound to kill an animal. Calculate how many bee stings are need to kill and animal in a function.");
+
+var victimWeight = prompt("Enter a weight in pounds.");
+victimWeight = Number(victimWeight);
+var beeStings = 8.666667
+var death = calculateDeathByBees(victimWeight, beeStings);
+    
+
