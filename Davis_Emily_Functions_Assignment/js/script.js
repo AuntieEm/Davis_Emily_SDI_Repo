1f327d0aa2b227a9d83735272dffc55164ee8185
AuntieEm <Emily.davis.5828@gmail.com>
2015-03-26 01:33:58 -0500

    Prompt for choice
    use while loop to validate and return value and print choice to console.

--- Davis_Emily_Functions_Assignment/js/script.js
+++ Davis_Emily_Functions_Assignment/js/script.js
@@ -6,12 +6,12 @@ Functions Assignment
 
 //alert("Javascript works!");   // test to make sure script works
 
-// todo 1 function
+// 1 function
 // todo functions must use arguments & parameters
 // todo value must be returned for each function
 // todo scope variables properly. don't put all variables in main code.
 // todo pass values through as arguments and out as returned value
-// todo validate all prompts using while loop
+// validate all prompts using while loop
 // todo console should print:
     // todo Power Ball numbers:  #, #, #, #, # with PB #
         // 5 random numbers between 1 & 59 + pb between 1 & 35
@@ -21,6 +21,30 @@ Functions Assignment
 
 
 console.log("Welcome to the Lottery!"); //intro line
+
+
+var lotteryChoice = prompt("Which lottery would you like to play?\n\nThe Florida Lottery or the Powerball?");   //prompt user for a lottery choice.
+
+
+function lotteryValidation(choice)  {   //call new function to validate lottery choice prompt only 1 argument
+    var timesClicked = 1;   // variable to determine how many times "ok" is clicked without the user typing anything in.
+
+    while (choice === "")   { // start while loop. if a choice is not made then...
+        choice = prompt("You must choose which lottery you would like to play.\n\n Florida or Powerball?"); //prompt user to make a choice
+
+        timesClicked++; //each time "OK" is clicked...
+
+        if (timesClicked === 2)    {    //conditional  if "ok" is clicked twice with out any user input then...
+            console.log("Since you can't make a decision, I shall choose for you.");    //print to console a choice has been made for you
+            console.log("You're playing the powerball.");   //print the automatic choice
+            break;  // ends loop after 2 clicks
+        }
+
+    }
+    return choice;  //passes value of choice over to main code
+}
+
+lotteryChoice = lotteryValidation(lotteryChoice);   // sends the value of lotteryChoice to value of lotteryValidation
 
 
 
