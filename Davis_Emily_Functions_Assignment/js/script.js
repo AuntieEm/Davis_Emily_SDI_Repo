9e09d6cca3fc11bef03294b961e4bf027dfb4873
AuntieEm <Emily.davis.5828@gmail.com>
2015-03-26 02:36:38 -0500

    Tidy up
    clean up order and placement of code

--- Davis_Emily_Functions_Assignment/js/script.js
+++ Davis_Emily_Functions_Assignment/js/script.js
@@ -4,31 +4,13 @@ March 26, 2015
 Functions Assignment
  */
 
-//alert("Javascript works!");   // test to make sure script works
-
-// 1 function
-// todo functions must use arguments & parameters
-// todo value must be returned for each function
-// todo scope variables properly. don't put all variables in main code.
-// todo pass values through as arguments and out as returned value
-// validate all prompts using while loop
-// todo console should print:
-    // todo Power Ball numbers:  #, #, #, #, # with PB #
-        // 5 random numbers between 1 & 59 + pb between 1 & 35
-    // todo FL lottery numbers:  #, #, #, #, #, #
-        // 6 random numbers between 1 & 53
-    //each random number must be different with the exception of pb#
-
-
-console.log("Welcome to the Lottery!"); //intro line
-
-
+/*-------------------- VARIABLES --------------------*/
 var lotteryChoice = prompt("Which lottery would you like to play?\n\nThe Florida Lottery or the Powerball?", "Florida");   //prompt user for a lottery choice.
 var flLottery;
 var pbLottery;
 var pbLotteryPb;
 
-
+/*-------------------- FUNCTIONS --------------------*/
 
 function lotteryValidation(choice)  {   //call new function to validate lottery choice prompt only 1 argument
     var timesClicked = 1;   // variable to determine how many times "ok" is clicked without the user typing anything in.
@@ -47,23 +29,10 @@ function lotteryValidation(choice)  {   //call new function to validate lottery 
     }
     return choice;  //passes value of choice over to main code
 }
-
 lotteryChoice = lotteryValidation(lotteryChoice);   // sends the value of lotteryChoice to value of lotteryValidation
-console.log("So, I see you've chosen to play the " + lotteryChoice + " lottery.");  // print choice the user made.
 
 
-
-
-
-
-
-
-
-
-
-
-
-    //Florida Lottery Function
+//Florida Lottery Function
 
     function randomFlNumGen(min, max, num) {
 
@@ -78,11 +47,6 @@ console.log("So, I see you've chosen to play the " + lotteryChoice + " lottery."
     }
     flLottery = randomFlNumGen(1, 53, 5);
    // console.log("Florida lottery numbers are..." + flLottery);
-
-
-
-
-
 
 
 //Powerball Function
@@ -112,5 +76,8 @@ pbLotteryPb = randomPbNumGen(1, 35, 1);
 //console.log("And your powerball number is..." + pbLotteryPb);
 
 
+/*-------------------- OUTPUT --------------------*/
 
+
+console.log("So, I see you've chosen to play the " + lotteryChoice + " lottery.");  // print choice the user made.
 (lotteryChoice === "Florida") ? console.log("Florida lottery numbers are..." + flLottery) : console.log("Powerball lottery numbers are..." + pbLottery + "And the powerball is..." + pbLotteryPb);
