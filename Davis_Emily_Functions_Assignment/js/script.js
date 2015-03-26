c0eeb6efc0724df811323256dc016de8bc934659
AuntieEm <Emily.davis.5828@gmail.com>
2015-03-26 01:15:36 -0500

    Begin Assignment
    create files and folders, make notes as to what is needed.

---
+++ Davis_Emily_Functions_Assignment/js/script.js
@@ -0,0 +1,27 @@
+/*
+Emily Davis
+March 26, 2015
+Functions Assignment
+ */
+
+//alert("Javascript works!");   // test to make sure script works
+
+// todo 1 function
+// todo functions must use arguments & parameters
+// todo value must be returned for each function
+// todo scope variables properly. don't put all variables in main code.
+// todo pass values through as arguments and out as returned value
+// todo validate all prompts using while loop
+// todo console should print:
+    // todo Power Ball numbers:  #, #, #, #, # with PB #
+        // 5 random numbers between 1 & 59 + pb between 1 & 35
+    // todo FL lottery numbers:  #, #, #, #, #, #
+        // 6 random numbers between 1 & 53
+    //each random number must be different with the exception of pb#
+
+
+console.log("Welcome to the Lottery!"); //intro line
+
+
+
+
